dbd819b33a94a920c675c023418f7b573d6fc655
benbrower <bbrower@smu.edu>
2020-09-08 12:06:13 -0500

    merge two sorted sub arrays into a single array

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+//find and remove minumum in a sorted array
 function findMinAndRemoveSorted(array) {
   return array.shift();
+}
+
+//merge two sorted sub arrays into a single array
+function merge(arr0, arr1) {
+  let sorted = [];
+
+  while (arr0.length != 0 && arr1.length != 0) {
+    if (arr0[0] < arr1[0]) {
+      sorted.push(findMinAndRemoveSorted(arr0));
+    } else {
+      sorted.push(findMinAndRemoveSorted(arr1));
+    }
+  }
+  return sorted.concat(arr0).concat(arr1);
 }
