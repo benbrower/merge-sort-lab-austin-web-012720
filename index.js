88f74343f90236fb73887f30766e09113f5190bc
Ben Brower <bbrower.dt@gmail.com>
2020-09-14 20:40:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function merge(firstHalf, secondHalf) {
   let sorted = [];
   while (firstHalf.length > 0 && secondHalf.length > 0){
     if(firstHalf[0] < secondHalf[0]){
-      sorted.push(findMinAndRemoveSorted(firstHalf[0]))
+      sorted.push(findMinAndRemoveSorted(firstHalf))
     } else {
-      sorted.push(findMinAndRemoveSorted(secondHalf[0]))
+      sorted.push(findMinAndRemoveSorted(secondHalf))
     }
   }
   return sorted.concat(firstHalf).concat(secondHalf);
