fc8ef3f02f78da884f9a0a764aba3b85ffdaad00
Ben Brower <bbrower.dt@gmail.com>
2020-09-14 20:32:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function findMinAndRemoveSorted(arr) {
-
+  let min = arr[0];
+  let index = 0;
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] < min){
+      min = arr[i];
+      index = i;
+    }
+  }
+  arr.splice(index, 1);
+  return min;
 }
 
 function merge(firstHalf, secondHalf) {
